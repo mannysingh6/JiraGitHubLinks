a476b2bcdc26d2dd0e0b90319b9314b3ddba743f
Manny Singh <msingh6@lenovo.com>
2021-07-07 09:32:38 -0400

    test commit

--- src/settings/views/Home.vue
+++ src/settings/views/Home.vue
@@ -21,7 +21,7 @@
             </v-list-item-icon>
 
             <v-list-item-content>
-              <v-list-item-title>Settings</v-list-item-title>
+              <v-list-item-title>Settings Test</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
           <v-list-item :key="1" link to="/commands">
